86a1537f0b082d2e16ff8ee635311edc9f223370
tzutseyang <tzutse.yang@gmail.com>
2019-12-22 18:06:25 +0800

    add main() in letterGrade

---
+++ src/main/java/letterGrade.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class letterGrade {
+
+    public static void main(String[] args) {
+        System.out.print("Enetr the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            int score = Integer.parseInt(br.readLine());
+            char grade = grade(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static char grade(int score) {
+        char grade;
+        if(score < 0 || score > 100) {
+            grade = 'X';
+        }else if(score >= 90) {
+            grade = 'A';
+        }else if(score >= 80) {
+            grade = 'B';
+        }else if(score >= 70) {
+            grade = 'C';
+        }else if(score >= 60) {
+            grade = 'D';
+        }else {
+            grade = 'F';
+        }
+        return grade;
+    }
+}
